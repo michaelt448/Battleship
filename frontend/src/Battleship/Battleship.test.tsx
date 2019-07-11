989db1988109b093a854aa7c6c432c52ddaeeb65
Michael <michaeltilman77@gmail.com>
2019-07-11 13:10:15 -0700

    set up of front end

---
+++ frontend/src/Battleship/Battleship.test.tsx
@@ -0,0 +1,7 @@
+import axios from "axios";
+import MockAdapter from "axios-mock-adapter";
+import { shallow } from "enzyme";
+import * as React from "react";
+import BattleShipApp from "./BattleshipApp";
+
+
---
+++ frontend/src/Battleship/BattleshipApp.tsx
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+// import axios from 'axios'
+
+export class App extends React.Component {
+  
+    public render() {
+      return (
+        <h1>Battle Ship</h1>
+      );
+    }
+}
+  
+  export default App;
